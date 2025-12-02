5a3a9911f411378e73b08b7859b2a6b6752e4ca2
yss1235-why <cofclanone1@gmail.com>
2025-12-02 19:50:41 +0530

    Create index.ts

---
+++ src/components/admin/index.ts
@@ -0,0 +1,5 @@
+export { OrdersPanel } from './OrdersPanel';
+export { TablesPanel } from './TablesPanel';
+export { TableLinksPanel } from './TableLinksPanel';
+export { MenuManagementPanel } from './MenuManagementPanel';
+export { StaffManagementPanel } from './StaffManagementPanel';
